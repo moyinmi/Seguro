cf86458578f4ae5a41dd216c72a9cc2ba3752479
moyinmi <38218683+moyinmi@users.noreply.github.com>
2023-09-11 08:03:52 +0100

    fix: updated the apply model

--- models/Apply.js
+++ models/Apply.js
@@ -5,52 +5,113 @@ const ApplySchema = new mongoose.Schema( {
         type:String,
         required: true
     },
-    lastName: {
+    lastName:{
         type:String,
-        max:50,
-        required:true,
+        required: true
     },
-    middleName: {
+    passport: {
         type:String,
-        max:50,
-        required:true,
+        required: true
+    },
+    thirdName:{
+        type:String,
+        required: true
+    },
+    rank: {
+        type:String,
+        required: true
+    },
+    gender: {
+        type:String,
+        required: true
+    },
+    dateOfBirth: {
+        type:String,
+        required: true
     },
     email: {
         type:String,
-        max:50,
-        required:true,
+        required: true
     },
-    passport : {
-        type: String,
-        required:true,
+    phoneNumber:{
+        type:String,
+        required: true
     },
-    phoneNumber : {
-        type: String,
-        required:true,
+    address:{
+        type:String,
+        required: true
     },
-    gender : {
-        type: String,
-        required:true,
+    city: {
+        type:String,
+        required: true
     },
-    dateOfBirth : {
-        type: String,
-        required:true,
+    state: {
+        type:String,
+        required: true
     },
-    rank : {
-        type: String,
-        required:true,
+    nextOfKin: {
+        type:String,
+        required: true
     },
-    address : {
-        type: String,
-        required:true,
+    nextOfKinNo: {
+        type:Number,
+        required: true
     },
-    city : {
-        type: String,
-        required:true,
+    dateOfEnlistment: {
+        type:String,
+        required: true
     },
-    state : {
-        type: String,
-        required:true,
+    dateOfRetirement: {
+        type:String,
+        required: true
+    },
+    ippisNo: {
+        type:Number,
+        required: true
+    },
+    apNo: {
+        type:Number,
+        required: true
+    },
+    forceNo: {
+        type:Number,
+        required: true
+    },
+    mss: {
+        type:Number,
+        required: true
+    },
+    salary: {
+        type:Number,
+        required: true
+    },
+    amount: {
+        type:Number,
+        required: true
+    },
+    duration: {
+        type:Number,
+        required: true
+    },
+    bank: {
+        type:String,
+        required: true
+    },
+    accountNumber: {
+        type:Number,
+        required: true
+    },
+    accountName: {
+        type:String,
+        required: true
+    },
+    bvn: {
+        type:Number,
+        required: true
+    },
+    bankCode: {
+        type:Number,
+        required: true
     },
     
     },
